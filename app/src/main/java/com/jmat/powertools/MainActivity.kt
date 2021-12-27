15733fb3c8dd8d87e75a7239a906170bd33966ea
Jordan Malfara <jordan.malfara@gmail.com>
2021-12-27 15:43:51 -0500

    Splashscreen

--- app/src/main/java/com/jmat/powertools/MainActivity.kt
+++ app/src/main/java/com/jmat/powertools/MainActivity.kt
@@ -2,18 +2,18 @@ package com.jmat.powertools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jmat.powertools.base.extensions.navigateDeeplink
-import com.jmat.powertools.base.extensions.navigateLauncherActions
 import com.jmat.powertools.modules.dashboard.DEEPLINK_DASHBOARD
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.textView).setOnClickListener {
-            navigateDeeplink(DEEPLINK_DASHBOARD)
+        val splashScreen = installSplashScreen()
+        splashScreen.setOnExitAnimationListener {
+           navigateDeeplink(DEEPLINK_DASHBOARD)
         }
+        setContentView(R.layout.activity_main)
     }
 }
