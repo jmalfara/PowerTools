845be900c07def96e3d6c26daa8de764c6b17a34
Jordan Malfara <jmalfara>
2022-09-13 11:45:13 -0600

    Version code bump

--- buildSrc/src/main/kotlin/com/jmat/powertools/ConfigData.kt
+++ buildSrc/src/main/kotlin/com/jmat/powertools/ConfigData.kt
@@ -3,6 +3,6 @@ package com.jmat.powertools
 object ConfigData {
     const val targetSdkVersion = 33
     const val minSdkVersion = 23
-    const val versionCode = 12
+    const val versionCode = 13
     const val versionName = "0.0.4"
 }
